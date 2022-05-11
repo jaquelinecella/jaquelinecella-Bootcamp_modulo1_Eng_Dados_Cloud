a70265a242eff354e408f2365dac561636a52c04
jaquelinecella2 <jaquelinecella@hotmail.com>
2022-05-11 10:23:02 -0300

    alterações das inscrições no arquivo delta_upsert

--- etl/02_delta_spark_upsert.py
+++ etl/02_delta_spark_upsert.py
@@ -25,69 +25,36 @@ from delta.tables import *
 logger.info("Produzindo novos dados...")
 enemnovo = (
     spark.read.format("delta")
-    .load("s3://datalake-jaque-igti-edc-tf/staging-zone/enem")
+    .load("s3://datalake-jaque-igti-tf/staging-zone/enem")
 )
 
 # Define algumas inscricoes (chaves) que serao alteradas
-inscricoes = [190001595656,
-            190001421546,
-            190001133210,
-            190001199383,
-            190001237802,
-            190001782198,
-            190001421548,
-            190001595657,
-            190001592264,
-            190001592266,
-            190001592265,
-            190001475147,
-            190001867756,
-            190001133211,
-            190001237803,
-            190001493186,
-            190001421547,
-            190001493187,
-            190001210202,
-            190001421549,
-            190001595658,
-            190002037437,
-            190001421550,
-            190001595659,
-            190001421551,
-            190001237804,
-            190001867757,
-            190001184600,
-            190001692704,
-            190001867758,
-            190002037438,
-            190001595660,
-            190001237805,
-            190001705260,
-            190001421552,
-            190001867759,
-            190001595661,
-            190001042834,
-            190001237806,
-            190001595662,
-            190001421553,
-            190001475148,
-            190001421554,
-            190001493188,
-            190002037439,
-            190001421555,
-            190001480442,
-            190001493189,
-            190001705261,
-            190001421556]
+#Modifiquei as inscrições para o ENEM 2020 e só adicionei 16 casos.
+inscricoes = [200006271946,
+            200001195856,
+            200001943954,
+            200001908998,
+            200001634757,
+            200003132410,
+            200001379770,
+            200001334237,
+            200006762554,
+            200005146210,
+            200004902048,
+            200006138472,
+            200005613689,
+            200004833505,
+            200004570764,
+            200001071590]
 
 
-logger.info("Reduz a 50 casos e faz updates internos no municipio de residencia")
+logger.info("Reduz a 16 casos e faz updates internos no municipio de residencia")
 enemnovo = enemnovo.where(enemnovo.NU_INSCRICAO.isin(inscricoes))
 enemnovo = enemnovo.withColumn("NO_MUNICIPIO_RESIDENCIA", lit("NOVA CIDADE")).withColumn("CO_MUNICIPIO_RESIDENCIA", lit(10000000))
 
 
 logger.info("Pega os dados do Enem velhos na tabela Delta...")
-enemvelho = DeltaTable.forPath(spark, "s3://datalake-jaque-igti-edc-tf/staging-zone/enem")
+enemvelho = DeltaTable.forPath(spark, "s3://datalake-jaque-igti-tf/staging-zone/enem")
 
 
 logger.info("Realiza o UPSERT...")
