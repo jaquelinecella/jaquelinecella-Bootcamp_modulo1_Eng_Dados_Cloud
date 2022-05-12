648aa567d683992af1e6eba7bfe080c6fde23876
jaquelinecella2 <jaquelinecella@hotmail.com>
2022-05-12 08:32:52 -0300

    alteração do json do arq IAM que está com erro

--- infraestructure/iam.tf
+++ infraestructure/iam.tf
@@ -121,12 +121,10 @@ resource "aws_iam_role_policy_attachment" "lambda_attach" {
  }
 
 
-
  resource "aws_iam_role_policy_attachment" "firehose_attach" {
    role       = aws_iam_role.firehose_role.name
    policy_arn = aws_iam_policy.firehose.arn
  }
-
 
 ###############
 ## GLUE ROLE ##
